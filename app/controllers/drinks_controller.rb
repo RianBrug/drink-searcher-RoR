165243f4a78e2da678001af583271006977c0253
Rian Brüggemann <riancvb@gmail.com>
2019-06-19 01:20:30 -0300

    distilled and extra_cold search filter

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,5 +1,22 @@
 class DrinksController < ApplicationController
   def index
-    @drinks = Drink.all.order(:created_at)
+    @drinks = filter(params["search"])
   end
+
+  def filter(query)
+   if params["extra_cold"]
+     return Drink.where(temperature: "extra_cold")
+   end
+   if params["distilled"]
+     return Drink.where(distilled: true)
+   end
+    return Drink.all unless query.present?
+    Drink.where('lower(name) = ?', "%#{query.downcase}%").or(Drink.where("lower(name) LIKE ?", "%#{query.downcase}%"))
+  end
+
+  def distilleds(query)
+    return Drink.all unless query.present?
+    Drink.where(query.downcase)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'drinks#index'
-
+  post "/", to: 'drinks#index'
+  post "search", to: 'drinks#search'
+  post "distilled", to: 'drinks#search'
+  post "extra_cold", to: 'drinks#search'
   resources :home, only: %i[index]
 end
